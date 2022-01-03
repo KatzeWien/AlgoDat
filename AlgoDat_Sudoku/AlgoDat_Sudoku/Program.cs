efc1d8b52e9bcb0a4b39a403ee78fee1290ee6ed
KatzeWien <daniela@sander.or.at>
2022-01-03 19:05:48 +0100

    Solution kopieren

---
+++ AlgoDat_Sudoku/AlgoDat_Sudoku/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoDat_Sudoku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    var sudoku = new char[,]
+            //    {
+            //{ '5', '3', '.', '.', '7', '.', '.', '.', '.' },
+            //{ '6', '.', '.', '1', '9', '5', '.', '.', '.' },
+            //{ '.', '9', '8', '.', '.', '.', '.', '6', '.' },
+            //{ '8', '.', '.', '.', '6', '.', '.', '.', '3' },
+            //{ '4', '.', '.', '8', '.', '3', '.', '.', '1' },
+            //{ '7', '.', '.', '.', '2', '.', '.', '.', '6' },
+            //{ '.', '6', '.', '.', '.', '.', '2', '8', '.' },
+            //{ '.', '.', '.', '4', '1', '9', '.', '.', '5' },
+            //{ '.', '.', '.', '.', '8', '.', '.', '7', '9' }
+            //    };
+            //    solveSudoku(sudoku);
+            int[,] field = new int[9, 9];
+            int[,] sudoku = new int[,]
+            {
+        { 7, 1, 0, 5, 3, 0, 4, 8, 2 },
+        { 2, 5, 0, 0, 0, 4, 0, 0, 0 },
+        { 8, 6, 0, 9, 7, 2, 1, 3, 0 },
+        { 1, 0, 0, 3, 6, 0, 0, 0, 0 },
+        { 0, 0, 2, 0, 0, 0, 6, 0, 0 },
+        { 0, 0, 0, 0, 9, 1, 0, 4, 3 },
+        { 3, 0, 0, 0, 0, 9, 0, 0, 0 },
+        { 0, 0, 1, 7, 0, 0, 0, 2, 6 },
+        { 4, 0, 7, 0, 0, 0, 3, 5, 0 }
+            };
+            Test test = new Test();
+            test.FillField(0, 0, 0, sudoku);
+            Console.ReadLine();
+        }
+        public static void solveSudoku(char[,] board)
+        {
+            if (board == null || board.Length == 0)
+                return;
+            solve(board);
+        }
+        private static bool solve(char[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j] == '.')
+                    {
+                        for (char c = '1'; c <= '9'; c++)
+                        {
+                            if (isValid(board, i, j, c))
+                            {
+                                board[i, j] = c;
+
+                                if (solve(board))
+                                    return true;
+                                else
+                                    board[i, j] = '.';
+                            }
+                        }
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        private static bool isValid(char[,] board, int row, int col, char c)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                //check row  
+                if (board[i, col] != '.' && board[i, col] == c)
+                    return false;
+                //check column  
+                if (board[row, i] != '.' && board[row, i] == c)
+                    return false;
+                //check 3*3 block  
+                if (board[3 * (row / 3) + i / 3, 3 * (col / 3) + i % 3] != '.' && board[3 * (row / 3) + i / 3, 3 * (col / 3) + i % 3] == c)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ AlgoDat_Sudoku/AlgoDat_Sudoku/Test.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoDat_Sudoku
+{
+    class Test
+    {
+        public bool CheckRow (int zeile, int wert, int[,] field)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (field[i, zeile] == wert)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool CheckColumn(int spalte, int wert, int[,] field)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (field[spalte, i] == wert)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool CheckBlock(int zeile, int spalte, int wert, int[,] field)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (field[spalte - (spalte % 3) + i, zeile - (zeile % 3) + j] == wert)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public bool CheckAll(int zeile, int spalte, int wert, int[,] field)
+        {
+            if (!CheckRow(zeile, wert, field))
+            {
+                //Console.WriteLine("Falsche Zeile");
+                return false;
+            }
+            if (!CheckColumn(spalte, wert, field))
+            {
+                //Console.WriteLine("Falsche Spalte");
+                return false;
+            }
+            if (!CheckBlock(zeile, spalte, wert, field))
+            {
+                //Console.WriteLine("Falscher Block");
+                return false;
+            }
+            //Console.WriteLine("Passt");
+            return true;
+        }
+
+        public void Solve()
+        {
+            int[,] sudoku = new int[,]
+            {
+        { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
+        { 6, 0, 0, 1, 9, 5, 0, 0, 0 },
+        { 0, 9, 8, 0, 0, 0, 0, 6, 0 },
+        { 8, 0, 0, 0, 6, 0, 0, 0, 3 },
+        { 4, 0, 0, 8, 0, 3, 0, 0, 1 },
+        { 7, 0, 0, 0, 2, 0, 0, 0, 6 },
+        { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
+        { 0, 0, 0, 4, 1, 9, 0, 0, 5 },
+        { 0, 0, 0, 0, 8, 0, 0, 7, 9 }
+            };
+
+            //CheckAll(0, 2, 5, sudoku);
+            for (int zeile = 0; zeile < 9; zeile++)
+            {
+                Console.WriteLine();
+                for (int spalte = 0; spalte < 9; spalte++)
+                {
+                    for (int wert = 1; wert < 10; wert++)
+                    {
+                        CheckAll(zeile, spalte, wert, sudoku);
+                        Console.Write(wert);
+                    }
+                }
+            }
+        }
+
+        public int[,] FillField(int wert, int row, int column, int[,] field)
+        {
+            int[,] currentField = new int[9, 9];
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    currentField[j, i] = field[j, i];
+                }
+            }
+            currentField[row, column] = wert;
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    if (currentField[x,y] == 0)
+                    {
+                        for (int input = 1; input <= 9; input++)
+                        {
+                            if (CheckAll(y,x,input,currentField))
+                            {
+                                FillField(input, x, y, currentField);
+                            }
+                        }
+                        return null;
+                    }
+                }
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Console.Write(currentField[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("--------");
+            return currentField;
+        }
+    }
+}
