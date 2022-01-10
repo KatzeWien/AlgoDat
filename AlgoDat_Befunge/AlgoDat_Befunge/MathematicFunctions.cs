6bceddf3ad39440d3ef3003abec76b19a6b76392
KatzeWien <daniela@sander.or.at>
2022-01-10 11:53:12 +0100

    add class diagramm and mathematic functions

---
+++ AlgoDat_Befunge/AlgoDat_Befunge/MathematicFunctions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgoDat_Befunge
+{
+    public class MathematicFunctions
+    {
+        public void Add()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Sub()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Multiplication()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Division()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Modulo()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Negetation()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
