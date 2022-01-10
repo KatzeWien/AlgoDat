b2403e5794d6b287ae27b44c45e6e811b3125a76
KatzeWien <daniela@sander.or.at>
2022-01-10 12:11:37 +0100

    add directions

---
+++ AlgoDat_Befunge/AlgoDat_Befunge/Directions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgoDat_Befunge
+{
+    public class Directions
+    {
+        public void MoveRight()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void MoveLeft()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void MoveUp()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void MoveDown()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void MoveRandom()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
